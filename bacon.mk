5eabc5c519ea879390f8e6324dc71288b05b0bcf
Martinusbe <martinusbe@gmail.com>
2016-09-21 14:14:00 +0200

    bacon: use oppo_common
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -261,4 +261,4 @@ PRODUCT_PACKAGES += \
     OnePlusDoze
 
 # Inherit from oppo-common
-#$(call inherit-product, device/oppo/common/common.mk)
+$(call inherit-product, device/oppo/common/common.mk)
